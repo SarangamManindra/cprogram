6a79806aa4430420e41056c54732cd984828e849
SarangamManindra <137697511+SarangamManindra@users.noreply.github.com>
2023-08-07 23:06:33 +0530

    Add files via upload

---
+++ multiplefiles/program1/task1/hexa/hexadecimal.c
@@ -0,0 +1,7 @@
+
+void hexademo()
+{
+	short int num1;
+	int num2;
+	long int num3;
+}
---
+++ multiplefiles/program1/task1/hexa/hexadecimal1.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include"hexadecimal.h"
+int main()
+{
+	printf("size of num1%d\n",num1);
+	printf("size of num2%d\n",num2);
+	printf("size of num3%d\n",num3);
+	return 0;
+}
